e76f8fd491ed96f2db9960d2dc829d293eaa8209
Jhonbic <162070596+Jhonbic@users.noreply.github.com>
2025-11-13 11:18:37 -0500

    Update GestionGanadoApplication.java

--- src/main/java/com/ganado/gestionganado/GestionGanadoApplication.java
+++ src/main/java/com/ganado/gestionganado/GestionGanadoApplication.java
@@ -13,22 +13,5 @@ public class GestionGanadoApplication {
         SpringApplication.run(GestionGanadoApplication.class, args);
     }
 
-    // Este método se ejecuta automáticamente al iniciar la app
-    /*@Bean
-    CommandLineRunner initDatabase(GanadoRepository ganadoRepository) {
-        return args -> {
-            /* Creamos un nuevo registro de ganado
-            Ganado vaca = new Ganado();
-            vaca.setNombre("Luna");
-            vaca.setRaza("Holstein");
-            vaca.setSexo("Hembra");
-            vaca.setFechaNacimiento(LocalDate.of(2022, 5, 10));
-            vaca.setNotas("Primera vaca registrada de prueba");
-
-            // Guardamos en la base de datos
-            ganadoRepository.save(vaca);
-
-            System.out.println("✅ Vaca registrada correctamente en la base de datos");
-        };
-    }*/
+    
 }
